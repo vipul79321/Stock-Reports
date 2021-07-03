c84d111c6c24bfbd9439d42308fbda713364c653
vipul79321 <vipul.gupta73921@gmail.com>
2021-07-03 21:20:48 +0530

    Fix: insufficient data points error covered. Description added in manifest.json

--- background.js
+++ background.js
@@ -40,6 +40,12 @@ function updateHistoricalData(stockUrl) {
             }
         }
 
+        if( CLOSE.length() < 100) {
+            console.log("Insufficient data points");
+            document.getElementById("Error").appendChild(createDOM("p", "Insufficient Data points"));
+            return;
+        }
+
         updateLTP();
         updateSimpleAverages();
         updateExponentialAverages();
